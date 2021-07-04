08948033e4472a40d4449e75f6b5312ea78d8198
gorakhkoli <gorakhkoli728@gmail.com>
2021-07-04 17:37:03 +0530

    remove comment

--- src/Consumer.java
+++ src/Consumer.java
@@ -17,7 +17,6 @@ public class Consumer implements Runnable
 				Thread.sleep(1000);
 			} catch (InterruptedException e)
 			{
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
